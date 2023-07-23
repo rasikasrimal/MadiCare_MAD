5a9a31f88a519ce9920d7d71b03ba2b4c2264329
Rasika srimal <77890327+rasikasrimal@users.noreply.github.com>
2023-07-23 22:16:54 +0530

    Update colors.dart

--- medicare/lib/constants/colors.dart
+++ medicare/lib/constants/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-
+//These are primary colours that should use in the app
 const blue = Color.fromARGB(255, 0, 94, 182);
 const mainColor = Color.fromARGB(255, 29, 35, 102);
