e52c4b31565c98398c8fba0a9b8f8cf1ca9a71d6
rasikasrimal <rasikacooldj620@gmail.com>
2023-09-21 22:38:30 +0530

    Important Adjustments and Optimizations

--- medicare/lib/medicine_shop/shop.dart
+++ medicare/lib/medicine_shop/shop.dart
@@ -1,109 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:medicare/constants/appbar.dart';
-import 'package:medicare/constants/colors.dart';
-import 'package:medicare/profile/Homepage.dart';
 
-class Shop extends StatefulWidget {
-  const Shop({Key? key}) : super(key: key);
+class MedicineItem {
+  final String name;
+  final double price;
+  final int quantityAvailable;
 
-  @override
-  _ShopState createState() => _ShopState();
+  MedicineItem({
+    required this.name,
+    required this.price,
+    required this.quantityAvailable,
+  });
 }
 
-class _ShopState extends State<Shop> {
-  int currentPage = 0;
-  final screens = [
-    const Home(),
-    //const HomePage(),
+class Shop extends StatelessWidget {
+  final List<MedicineItem> items = [
+    MedicineItem(name: 'Aspirin', price: 5.99, quantityAvailable: 100),
+    MedicineItem(name: 'Ibuprofen', price: 4.49, quantityAvailable: 80),
+    MedicineItem(name: 'Antacid', price: 7.99, quantityAvailable: 60),
+    MedicineItem(name: 'Cough Syrup', price: 8.99, quantityAvailable: 40),
+    MedicineItem(name: 'Vitamin C', price: 9.99, quantityAvailable: 120),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar('Medicine Shop'),
-      backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      color: mainColor,
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(20),
-                      child: Text(
-                        'Medicine Shop',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontFamily: 'Helvetica',
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  // Your existing content here
-                  // Add your widgets below this comment
-                ],
-              ),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text('Medicine Shop'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        elevation: 0,
-        currentIndex: currentPage,
-        onTap: (index) {
-          setState(() {
-            currentPage = index;
-          });
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return ListTile(
+            title: Text(item.name),
+            subtitle: Text('Price: \$${item.price.toStringAsFixed(2)}\n'
+                'Quantity Available: ${item.quantityAvailable}'),
+            trailing: ElevatedButton(
+              onPressed: () {
+                // Implement item purchase logic here
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Purchase Confirmation'),
+                      content: Text('You have purchased ${item.name}.'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Buy'),
+            ),
+          );
         },
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: [
-          BottomNavigationBarItem(
-            icon: currentPage == 0
-                ? const Icon(Icons.home, color: mainColor)
-                : const Icon(Icons.home, color: Colors.black),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: currentPage == 1
-                ? const Icon(
-                    Icons.person,
-                    color: mainColor,
-                  )
-                : const Icon(
-                    Icons.person,
-                    color: Colors.black,
-                  ),
-            label: "Profile",
-          ),
-          BottomNavigationBarItem(
-            icon: currentPage == 2
-                ? const Icon(Icons.chat, color: mainColor)
-                : const Icon(Icons.chat, color: Colors.black),
-            label: "Chat",
-          ),
-          BottomNavigationBarItem(
-            icon: currentPage == 3
-                ? const Icon(Icons.calendar_month, color: mainColor)
-                : const Icon(Icons.calendar_month, color: Colors.black),
-            label: "Calendar",
-          ),
-          BottomNavigationBarItem(
-            icon: currentPage == 4
-                ? const Icon(Icons.view_list, color: mainColor)
-                : const Icon(Icons.view_list, color: Colors.black),
-            label: "List",
-          ),
-        ],
       ),
     );
   }
